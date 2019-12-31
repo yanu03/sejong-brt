3f2686f12c73a2c445dc5f2ee422db112874e5bb
hrpfs <hrpfsn@gmail.com>
2019-12-31 09:47:09 +0900

    [기초정보_승무사원관리] CUD 추가

--- brt/src/main/java/com/tracom/brt/domain/BM0108/BM0108Mapper.java
+++ brt/src/main/java/com/tracom/brt/domain/BM0108/BM0108Mapper.java
@@ -7,4 +7,8 @@ import com.chequer.axboot.core.mybatis.MyBatisMapper;
  
 public interface BM0108Mapper extends MyBatisMapper {
 	List<EplyInfoVO> BM0108G0S0(String filter);
+	int BM0108F0I0(EplyInfoVO vo);
+	int BM0108F0U0(EplyInfoVO vo);
+	int BM0108G0D0(EplyInfoVO vo);
+	
 }
--- brt/src/main/java/com/tracom/brt/domain/BM0108/BM0108Service.java
+++ brt/src/main/java/com/tracom/brt/domain/BM0108/BM0108Service.java
@@ -15,8 +15,33 @@ public class BM0108Service extends BaseService<EplyInfoVO, String> {
 	@Inject
 	private BM0108Mapper mapper;
 	
+	//Select
     public List<EplyInfoVO> BM0108G0S0(RequestParams<EplyInfoVO> requestParams) {
         return mapper.BM0108G0S0(requestParams.getString("filter"));
+    }
+    
+    //Insert
+    public String BM0108F0I0(EplyInfoVO vo) {
+    	mapper.BM0108F0I0(vo);
+    	return vo.getEplyId();
+    }
+    
+    //Update
+    public boolean BM0108F0U0(EplyInfoVO vo) {
+    	if(mapper.BM0108F0U0(vo) > 0) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+    
+    //Delete
+    public boolean BM0108G0D0(EplyInfoVO vo) {
+    	if(mapper.BM0108G0D0(vo) > 0) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
     
 }
