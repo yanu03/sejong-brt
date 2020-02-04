08b9696f658da582555131bbc93cef604d922c63
hrpfs <hrpfsn@gmail.com>
2020-02-04 21:48:52 +0900

    [계약관리] 계약관리 모달 추가

---
+++ brt/src/main/webapp/assets/js/common/BM0301_modal.js
@@ -0,0 +1,119 @@
+var fnObj = {};
+var ACTIONS = axboot.actionExtend(fnObj, {
+    PAGE_CLOSE: function (caller, act, data) {
+        if (parent) {
+            parent.axboot.modal.close();
+        }
+    },
+    PAGE_SEARCH: function (caller, act, data) {
+        axboot.ajax({
+            type: "GET",
+            url: "/api/v1/BM0301G0S0",
+            data: caller.searchView.getData(),
+            callback: function (res) {
+                caller.gridView01.setData(res);
+            }
+        });
+        return false;
+    },
+    PAGE_CHOICE: function (caller, act, data) {
+        var list = caller.gridView01.getData("selected");
+        if (list.length > 0) {
+            if (parent && parent.axboot && parent.axboot.modal) {
+                parent.axboot.modal.callback(list[0]);
+            }
+        } else {
+            alert(LANG("ax.script.requireselect"));
+        }
+    },
+    ITEM_CLICK: function (caller, act, data) {
+    },
+});
+
+var CODE = {};
+
+// fnObj 기본 함수 스타트와 리사이즈
+fnObj.pageStart = function () {
+    var _this = this;
+    
+    _this.pageButtonView.initView();
+    _this.searchView.initView();
+    _this.gridView01.initView();
+
+    ACTIONS.dispatch(ACTIONS.PAGE_SEARCH);
+};
+
+fnObj.pageResize = function () {
+
+};
+
+fnObj.pageButtonView = axboot.viewExtend({
+    initView: function () {
+        axboot.buttonClick(this, "data-page-btn", {
+            "search": function () {
+                ACTIONS.dispatch(ACTIONS.PAGE_SEARCH);
+            },
+            "choice": function () {
+                ACTIONS.dispatch(ACTIONS.PAGE_CHOICE);
+            },
+            "close": function () {
+                ACTIONS.dispatch(ACTIONS.PAGE_CLOSE);
+            }
+        });
+    }
+});
+
+//== view 시작
+/**
+ * searchView
+ */
+fnObj.searchView = axboot.viewExtend(axboot.searchView, {
+    initView: function () {
+        this.target = $(document["searchView0"]);
+        this.target.attr("onsubmit", "return ACTIONS.dispatch(ACTIONS.PAGE_SEARCH);");
+        this.filter = $("#filter");
+    },
+    setPageNumber: function (pageNumber) {
+        this.pageNumber = pageNumber;
+        ACTIONS.dispatch(ACTIONS.PAGE_SEARCH);
+    },
+    getData: function () {
+        return {
+            filter: this.filter.val()
+        }
+    }
+});
+
+/**
+ * gridView
+ */
+fnObj.gridView01 = axboot.viewExtend(axboot.gridView, {
+    initView: function () {
+        var _this = this;
+
+        this.target = axboot.gridBuilder({
+            showLineNumber: true,
+            frozenColumnIndex: 0,
+            target: $('[data-ax5grid="grid-view-01"]'),
+            columns: [
+            	{key: "conNo", label: ADMIN("ax.admin.BM0301F0.conno"), width: 120},
+                {key: "conId", label: ADMIN("ax.admin.BM0301F0.conid"), width: 120},
+                {key: "conNm", label: ADMIN("ax.admin.BM0301F0.connm"), width: 120},
+                {key: "conFstDate", label: ADMIN("ax.admin.BM0301F0.confd"), width: 120},
+                {key: "conStDate", label: ADMIN("ax.admin.BM0301F0.consd"), width: 120},
+                {key: "conEdDate", label: ADMIN("ax.admin.BM0301F0.coned"), width: 120},
+                {key: "confirmYn", label: ADMIN("ax.admin.BM0301F0.confirmyn"), width: 70},
+                {key: "custId", label: ADMIN("ax.admin.BM0301F0.custid"), width: 120},
+                {key: "suppAmt", label: ADMIN("ax.admin.BM0301F0.suppamt"), width: 120},
+                {key: "vatAmt", label: ADMIN("ax.admin.BM0301F0.vatamt"), width: 70},
+                {key: "remark", label: ADMIN("ax.admin.BM0301F0.remark"), width: 200},
+            ],
+            body: {
+                onClick: function () {
+                    this.self.select(this.dindex);
+                    ACTIONS.dispatch(ACTIONS.ITEM_CLICK, this.item);
+                }
+            }
+        });
+    }
+});
