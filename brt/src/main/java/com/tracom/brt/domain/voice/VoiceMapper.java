a50dd6fe32f61262edc87f34d6a39df45bad5948
hjkim <hjkim@tracom.kr>
2020-02-26 17:14:56 +0900

    - 음성 TTS 도움말 완성

--- brt/src/main/java/com/tracom/brt/domain/voice/VoiceMapper.java
+++ brt/src/main/java/com/tracom/brt/domain/voice/VoiceMapper.java
@@ -1,7 +1,11 @@
 package com.tracom.brt.domain.voice;
 
+import java.util.List;
+import java.util.Map;
+
 import com.chequer.axboot.core.mybatis.MyBatisMapper;
 
 public interface VoiceMapper extends MyBatisMapper {
 	int checkVoiceOrganization(String vocId);
+	List<Map<String, Object>> selectTtsHelp();
 }
